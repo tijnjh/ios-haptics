335b78fe994d1776d339f893467477cfc3850c20
tijn <tijn@tijn.dev>
2025-06-10 14:29:03 +0200

    docs: add license

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "tsdown";
 
+import pkg from "./package.json";
+
+const banner = `/**
+* ${pkg.name} v${pkg.version}
+* tijn.dev
+* @license MPL-2.0
+**/`;
+
 export default defineConfig({
   entry: ["./src/index.ts"],
   platform: "neutral",
+  outputOptions: { banner },
 });
